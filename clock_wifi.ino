85cab0dbc7c4273100b4b46ef0920d2d32daf331
nicolas-f <[omitted]>
2020-08-12 16:51:15 +0200

    ntp clock

---
+++ clock_wifi.ino
@@ -0,0 +1,110 @@
+#include "AtWiFi.h"    // https://wiki.seeedstudio.com/Wio-Terminal-Network-Overview/#libraries-installation
+#include "settings.h"
+// This file contains
+//const char* ssid = "WIFISSID";
+//const char* password =  "WIFIPWD";
+
+#include <NTPClient.h> //https://github.com/arduino-libraries/NTPClient
+#include <WiFiUdp.h>
+
+
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
+
+#define TFT_GREY 0x5AEB
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+
+static uint8_t conv2d(const char* p); // Forward declaration needed for IDE 1.6.x
+
+uint8_t hh = -99, mm = -99, ss = -99;
+
+byte omm = 99, oss = 99;
+byte xcolon = 0, xsecs = 0;
+unsigned int colour = 0;
+
+
+uint32_t targetTime = 0;                    // for next 1 second timeout
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 7200, 3600000);
+ 
+void setup() {
+  Serial.begin(115200);
+  tft.init();
+  tft.setRotation(3);
+  tft.fillScreen(TFT_BLACK);
+
+  tft.setTextSize(1);
+  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+  while(!Serial); // Wait for Serial to be ready
+  delay(1000);
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(2000);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+  Serial.print("IP Address: ");
+  Serial.println (WiFi.localIP()); // prints out the device's IP address
+  timeClient.begin();
+  targetTime = millis() + 1000;
+}
+ 
+void loop() { 
+  if (targetTime < millis()) {
+    // Set next update for 1 second later
+    targetTime = millis() + 1000;
+    
+    timeClient.update();
+    //Serial.println(timeClient.getFormattedTime());
+    // parse time
+    hh = timeClient.getHours();
+    mm = timeClient.getMinutes();
+    ss = timeClient.getSeconds();
+    
+    // Update digital time
+    int xpos = 0;
+    int ypos = 85; // Top left corner ot clock text, about half way down
+    int ysecs = ypos + 24;
+  
+    if (omm != mm) { // Redraw hours and minutes time every minute
+      omm = mm;
+      // Draw hours and minutes
+      if (hh < 10) xpos += tft.drawChar('0', xpos, ypos, 8); // Add hours leading zero for 24 hr clock
+      xpos += tft.drawNumber(hh, xpos, ypos, 8);             // Draw hours
+      xcolon = xpos; // Save colon coord for later to flash on/off later
+      xpos += tft.drawChar(':', xpos, ypos - 8, 8);
+      if (mm < 10) xpos += tft.drawChar('0', xpos, ypos, 8); // Add minutes leading zero
+      xpos += tft.drawNumber(mm, xpos, ypos, 8);             // Draw minutes
+      xsecs = xpos; // Sae seconds 'x' position for later display updates
+    }
+    if (oss != ss) { // Redraw seconds time every second
+      oss = ss;
+      xpos = xsecs;
+  
+      if (ss % 2) { // Flash the colons on/off
+        tft.setTextColor(0x39C4, TFT_BLACK);        // Set colour to grey to dim colon
+        tft.drawChar(':', xcolon, ypos - 8, 8);     // Hour:minute colon
+        xpos += tft.drawChar(':', xsecs, ysecs, 6); // Seconds colon
+        tft.setTextColor(TFT_YELLOW, TFT_BLACK);    // Set colour back to yellow
+      }
+      else {
+        tft.drawChar(':', xcolon, ypos - 8, 8);     // Hour:minute colon
+        xpos += tft.drawChar(':', xsecs, ysecs, 6); // Seconds colon
+      }
+  
+      //Draw seconds
+      if (ss < 10) xpos += tft.drawChar('0', xpos, ysecs, 6); // Add leading zero
+      tft.drawNumber(ss, xpos, ysecs, 6);                     // Draw seconds
+    }
+  }
+  delay(125);
+}
